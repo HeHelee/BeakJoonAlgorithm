0d931cb278105db46eb38c9e6a440641a5fc6c07
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2023-11-23 18:10:13 +0900

    [Silver I] Title: 숨바꼭질, Time: 164 ms, Memory: 18508 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -3,63 +3,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main
-{
+public class Main {
+	static int visited[] = new int[100001];
 	static int N;
 	static int K;
-	
-	static int visited[] = new int[100001];
-	
-	// X-1, X+1
-	// 2*X
-	public static void main(String[] args) throws IOException
-	{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String input = br.readLine();
-		String[] inputs = input.split(" ");
-		
-		N = Integer.valueOf(inputs[0]);
-		K = Integer.valueOf(inputs[1]);
-		
-		int result = bfs(N);
-		System.out.println(result);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 수빈이 위치
+		K = Integer.parseInt(st.nextToken()); // 동생 위치
+	    int result = bfs(N);
+	    System.out.println(result);
 	}
 
-	private static int  bfs(int node)
-	{
-		Queue<Integer> queue = new LinkedList<Integer>();
-		
-		queue.add(node);
-		int index = node;
-		int n = 0;
+	private static int bfs(int nodeIndex) {
+		Queue<Integer> q = new LinkedList<>();
+
+		q.offer(nodeIndex); // 큐에 저장
+		int index = nodeIndex;
+		int now = 0;
 		visited[index] = 1;
-		while (queue.isEmpty() == false)
-		{
-			n = queue.remove();
-			
-			if (n == K)
-			{
-				return visited[n]-1;
+		while (!q.isEmpty()) {
+			now = q.poll(); // 팝
+
+			if (now == K) {
+				return visited[now] - 1;
+			}
+
+			if (now-1 >= 0 && visited[now-1] == 0) {
+				visited[now - 1] = visited[now] + 1;
+				q.offer(now - 1);
+			}
+			if (now + 1 <= 100000 && visited[now+1]==0) {
+				visited[now + 1] = visited[now] + 1;
+				q.offer(now + 1);
+			}
+			if (now*2 <= 100000 && visited[now*2]==0) {
+				visited[now * 2] = visited[now] + 1;
+				q.offer(now * 2);
 			}
 			
-			if (n-1>=0 && visited[n-1] == 0)
-			{
-				visited[n-1] = visited[n]+1;
-				queue.add(n-1);
-			}
-			if (n+1 <= 100000 && visited[n+1] == 0)
-			{
-				visited[n+1] = visited[n]+1;
-				queue.add(n+1);
-			}
-			if (2*n <= 100000 && visited[2*n] == 0)
-			{
-				visited[2*n] = visited[n] + 1;
-				queue.add(2*n);
-			}
 		}
 		return -1;
 	}
+
 }
