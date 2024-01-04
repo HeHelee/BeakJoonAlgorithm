d4956ce703984e5688cb2d9fb51701f2d85cacf3
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-01-04 19:47:38 +0900

    [Silver V] Title: 돌 게임, Time: 124 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,22 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		if(N%2 == 0) {
-			System.out.println("CY");
+		int N = Integer.parseInt(br.readLine()); // 돌 갯수 입력받기
+		int dp[] = new int[1001];
+		// dp에 값 저장
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 1;
+
+		for (int i = 4; i <= N; i++) {
+			dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
 		}
-		else if (N%2 == 1) {
-			System.out.println("SK");
+		if (dp[N] % 2 == 0) {
+			System.out.print("CY");
 		}
-		
-		
+		else {
+			System.out.print("SK");
+		}
 
 	}
 
