e8074dc78820ba2d23130b66b74a01d14cadc462
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-01-11 17:45:00 +0900

    [Silver III] Title: 파도반 수열, Time: 132 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static long dp[] = new long[101];
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        Pado();
+        
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			sb.append(dp[Integer.parseInt(br.readLine())]).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+	private static void Pado() {
+		
+		dp[1] = 1;
+		dp[2] = 1;
+		dp[3] = 1;
+
+		for (int i = 4; i < 101; i++) {
+			dp[i] = dp[i - 2] + dp[i - 3];
+		}
+	}
+}
