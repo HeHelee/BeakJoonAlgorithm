80e4dd6dd163f64920baebd56001b66efb0bf1eb
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-01-23 00:10:02 +0900

    [Bronze II] Title: 소수 찾기, Time: 212 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,33 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-static int count = 0;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int num = 0;
-		
-		for (int i = 0; i<N; i++) {
-			num = sc.nextInt();
-			if(CheckIsPrimeNumber(num))
-				count++;
-		}
+        int count = 0;
+        
+		for (int i = 0; i < N; i++) {
+			int prime = sc.nextInt();
+			if (CheckPrime(prime)) {
+				count ++;
+		    }
+		} 
 		System.out.println(count);
-		
 	}
 
-	private static boolean CheckIsPrimeNumber(int num) {
-		if(num<2)
+	private static boolean CheckPrime(int prime) {
+		if (prime == 1)
 			return false;
-		for (int i = 2; i*i <= num; i++) {
-			if(num % i == 0) {
+		for (int j = 2; j<prime; j++) {
+			if (prime % j == 0)
 				return false;
-			}
 		}
 		return true;
-		
-		
-		
 	}
 }
- 
+
