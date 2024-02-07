69a0fb0b851b2d239493e1d2e649703d5e03c815
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-02-07 14:29:37 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int N; //컴퓨터 갯수
+	public static int M; //간선 갯수
+	public static boolean graph [][]; //연결 정보
+	public static boolean visit[]; //방문 배열
+	public static int answer; //답
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		//0. 입력 및 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine());
+	    M = Integer.parseInt(br.readLine());
+	    
+		
+	    graph = new boolean[N+1][N+1];
+	    visit = new boolean[N+1];
+	    
+	    //1. graph에 연결 정보 채우기
+		int x, y;
+		for (int i = 0; i<M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			graph[x][y] = true;
+			graph[y][x] = true;
+		}
+		
+		//2. dfs(재귀함수 호출)
+		dfs(1); //1번 컴퓨터부터 해주겠다.
+		
+		//3 출력
+		bw.write(String.valueOf(answer - 1)); //몇개의 컴퓨터가 감염이 됐는지
+		br.close();
+		bw.close();
+		
+
+	}
+
+	private static void dfs(int depth) {
+		visit[depth] = true;
+		answer++;
+		
+		for (int i = 1; i<=N; i++) {
+			if (!visit[i] && graph[depth][i] == true) {
+				dfs(i);
+			}
+		}
+		
+	}
+
+}
