8a321e99a83fc93a1e40bfb9de7217cfbdbb6a46
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-02-09 23:11:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 360 ms, Memory: 21800 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,82 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>(); // 배열 생성
-	static int count;
-	static boolean visited[];
-	static boolean visited2[];
+    final static int MAX = 1000 + 10;
+    static boolean graph [][];
+    static boolean visited [];
+    static ArrayList<Integer> queue;
+    static int N,M,V;
 
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 정점의 갯수
-		int M = Integer.parseInt(st.nextToken()); // 간선의 갯수
-		int V = Integer.parseInt(st.nextToken()); // 시작 정점
-		// 방문배열 생성
-		visited = new boolean[N + 1];
-		visited2 = new boolean[N + 1];
-		// 그래프 초기화
-		for (int i = 0; i < N + 1; i++) {
-			graph.add(new ArrayList<>());
-		}
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken()); // 간선1
-			int v = Integer.parseInt(st.nextToken()); // 간선2
-			graph.get(u).add(v); // 그래프 연결
-			graph.get(v).add(u);
+    public static void main(String[] args) throws IOException {
+        //0.입력 및 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		}
-		// 오름차순 해준다.
-		for (int i = 0; i < graph.size(); i++) {
-			Collections.sort(graph.get(i));
-		}
-		count = 1;
-		dfs(V); 
-		System.out.println();
-	    bfs(V);
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	private static void bfs(int nodeIndex) {
-		Queue<Integer> q = new LinkedList<>();
-		
-		q.offer(nodeIndex); //팝
-		visited2[nodeIndex] = true;
-		System.out.print(nodeIndex + " ");
-		
-		while(!q.isEmpty()) {
-			int a = q.poll(); //삽입
-			//인접 노드 찾기
-			for(int newNode:graph.get(a)) {
-				if(visited2[newNode] == false) {
-				q.offer(newNode);
-		        System.out.print(newNode + " ");
-				visited2[newNode] = true;
-				}
-			}
-		}
-		
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
 
-	}
+        graph = new boolean[MAX][MAX];
+        visited = new boolean[MAX];
 
-	private static void dfs(int nodeIndex) {
-		System.out.print(nodeIndex + " ");
-		visited[nodeIndex] = true;
-		// 인접 노드 찾기 
-		for (int newNode : graph.get(nodeIndex)) {
-			if (visited[newNode] == false) {
-				dfs(newNode);
-			}
-		}
+        //1.graph에 연결 정보 채우기
+        for (int i = 0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x][y] = true;
+            graph[y][x] = true;
+        }
 
-	}
+        //2.dfs(재귀함수 호출)
+        dfs(V);
+        System.out.println();
+        
+        //3.bfs
+        bfs();
 
+    }
+
+    private static void bfs() {
+        //0.초기화 작업
+        queue = new ArrayList<>();
+        visited = new boolean[MAX];
+
+        queue.add(V);
+        visited[V] = true;
+
+        while(!queue.isEmpty()) {
+            int idx = queue.remove(0);
+            System.out.print(idx + " ");
+
+            for (int i = 1; i<=N; i++) {
+                if (visited[i] == false && graph[idx][i] == true) {
+                    queue.add(i);
+                    visited[i] = true;
+                }
+            }
+
+        }
+
+    }
+
+    private static void dfs(int depth) {
+        visited[depth] = true;
+        System.out.print(depth + " ");
+        for (int i = 1; i<=N; i++) {
+            if (visited[i] == false && graph[depth][i] == true) {
+                dfs(i);
+            }
+        }
+    }
 }
