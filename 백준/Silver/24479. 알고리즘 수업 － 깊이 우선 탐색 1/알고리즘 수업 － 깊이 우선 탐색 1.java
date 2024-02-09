bf0a61fff460b1ac5364cbbf90d2658e6ad85a94
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-02-09 21:09:56 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1064 ms, Memory: 177904 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -1,71 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
+    final static int MAX = 10000000 + 10;
+    static int N, M, R;
+    static boolean [] visited;
+    static int [] answer;
+    static ArrayList<Integer>[] graph;
+    static int order;
 
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>(); // 정점들의 정보를													// 기록할 그래프
-	static int[] visited; // 방문할 정점들을 기록할 배열
-	static int count; // 방문 순서
+    public static void main(String[] args) throws IOException {
+        // 0.입력 및 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); //정점
+        int M = Integer.parseInt(st.nextToken()); //간선
+        int R = Integer.parseInt(st.nextToken()); //시작 정점
 
-		int M = Integer.parseInt(st.nextToken()); // 정점의 수
-		int N = Integer.parseInt(st.nextToken()); // 간선의 수
-		int R = Integer.parseInt(st.nextToken()); // 시작 정점
+        // 1.graph에 연결 정보 채우기
+        graph = new ArrayList[MAX];
+        for (int i = 1; i<=N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        visited = new boolean[MAX]; //방문 배열
+        answer = new int[MAX]; //출력배열
+        order = 1; //현재 dfs를 방문하는 순서
 
-		visited = new int[M + 1];
+        for (int i = 0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x].add(y);
+            graph[y].add(x);
+        }
+        // 2.오름차순 정렬
+        for (int i = 1; i<=N; i++) {
+            Collections.sort(graph[i]);
+        }
 
-		// graph의 인덱스를 정점의 개수만큼 만들어줌
-		for (int i = 0; i < M+1; i++) {
-			graph.add(new ArrayList<>());
-		}
+        // 3.dfs(재귀함수 호출)
+        dfs(R);
+        // 4.출력
+        for (int i = 1; i<=N; i++) {
+            bw.write(String.valueOf(answer[i]));
+            bw.newLine();
+        }
+        bw.close();
+        br.close();
+    }
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
+    private static void dfs(int depth) {
+        visited[depth] = true;
+        answer[depth] = order;
+        order ++;
 
-			// 그래프를 서로 연결해준다.
-			graph.get(u).add(v);
-			graph.get(v).add(u);
-		}
-		// 오름 차순 해준다.
-		for (int i = 0; i < graph.size(); i++) {
-			Collections.sort(graph.get(i));
-		}
-		// 시작 정점도 순서에 포함하므로 count 1추가
-		count = 1;
+        for (int i = 0; i<graph[depth].size(); i++) {
+            int next = graph[depth].get(i);
+            if (visited[next] == false) {
+                dfs(next);
+            }
+        }
 
-		// 깊이 우선 탐색 재귀 시작
-		dfs(R);
-		
-		for (int i = 1; i< visited.length; i++) {
-			sb.append(visited[i]).append("\n");
-		}
-		System.out.println(sb);
-
-	}
-
-	private static void dfs(int nodeIndex) {
-		visited[nodeIndex] = count;// 현재 방문하고 있는 정점에 순서 저장
-
-		// 노드에 인접한 노드 찾기
-	    for(int i = 0; i<graph.get(nodeIndex).size();i++) {
-	    	int newNode = graph.get(nodeIndex).get(i);
-	    	if(visited[newNode] == 0) {
-	    		count++;
-	    	    dfs(newNode);
-	    	}
-	    }
-
-	}
+    }
 }
