68a0302b52a4f102d0fc88b57e468febf304982f
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-03-20 17:20:34 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1096 ms, Memory: 93800 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -4,66 +4,63 @@ import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    final static int MAX = 10000000 + 10;
-    static int N, M, R;
-    static boolean [] visited;
-    static int [] answer;
-    static ArrayList<Integer>[] graph;
+    static int MAX = 100000 + 100;
+    static int N,M,R;
+    static ArrayList<Integer>[] map;
+    static int visited[];
     static int order;
 
     public static void main(String[] args) throws IOException {
-        // 0.입력 및 초기화
+        //0. 입력 및 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); //정점
-        int M = Integer.parseInt(st.nextToken()); //간선
-        int R = Integer.parseInt(st.nextToken()); //시작 정점
 
-        // 1.graph에 연결 정보 채우기
-        graph = new ArrayList[MAX];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        //1. 맵 정보 입력하기
+        map = new ArrayList[MAX];
         for (int i = 1; i<=N; i++) {
-            graph[i] = new ArrayList<>();
+            map[i] = new ArrayList<>();
         }
-        visited = new boolean[MAX]; //방문 배열
-        answer = new int[MAX]; //출력배열
-        order = 1; //현재 dfs를 방문하는 순서
+
+        visited = new int[MAX];
+        order = 1;
 
         for (int i = 0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            graph[x].add(y);
-            graph[y].add(x);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            map[u].add(v);
+            map[v].add(u);
         }
-        // 2.오름차순 정렬
+        //2.오름차순
         for (int i = 1; i<=N; i++) {
-            Collections.sort(graph[i]);
+            Collections.sort(map[i]);
         }
 
-        // 3.dfs(재귀함수 호출)
+        //3. dfs호출
         dfs(R);
-        // 4.출력
+
+        //4. 출력
         for (int i = 1; i<=N; i++) {
-            bw.write(String.valueOf(answer[i]));
+            bw.write(String.valueOf(visited[i]));
             bw.newLine();
         }
         bw.close();
         br.close();
+
     }
 
     private static void dfs(int depth) {
-        visited[depth] = true;
-        answer[depth] = order;
+        visited[depth] = order;
         order ++;
-
-        for (int i = 0; i<graph[depth].size(); i++) {
-            int next = graph[depth].get(i);
-            if (visited[next] == false) {
+        for (int i = 0; i<map[depth].size(); i++) {
+            int next = map[depth].get(i);
+            if (visited[next] == 0)
                 dfs(next);
-            }
         }
-
     }
 }
