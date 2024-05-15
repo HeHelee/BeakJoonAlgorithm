b818313a92a5a23ba23719a892b51a89e3fd1cb6
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-05-16 02:03:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,84 +1,78 @@
+import com.sun.jdi.IntegerValue;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-    static int MAX = 25 + 10;
     static int N;
-    static int map[][];
+    static int graph[][];
     static boolean visited[][];
+    static int count;
     static int answer;
     static int dirX[] = {-1,1,0,0};
     static int dirY[] = {0,0,-1,1};
-    static int count;
+    static int MAX = 25 + 10;
+
+
     public static void main(String[] args) throws IOException {
         //0.입력 및 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         N = Integer.parseInt(br.readLine());
-        map = new int[MAX][MAX];
+
+        count = 0;
+        graph = new int[MAX][MAX];
         visited = new boolean[MAX][MAX];
 
-
-
-        //1.맵 정보 입력하기
+        //1.그래프 정보 입력
         for (int i = 1; i<=N; i++) {
-            String[] input = br.readLine().split("");
+            String input[] = br.readLine().split("");
             for (int j = 1; j<=N; j++) {
-                map[i][j] = Integer.parseInt(input[j-1]);
+                graph[i][j] = Integer.parseInt(input[j-1]);
             }
         }
 
         //2.dfs 호출
-
-        ArrayList<Integer>arr = new ArrayList<>();
-        answer = 0;
+        ArrayList<Integer> arr = new ArrayList<>();
         for (int i = 1; i<=N; i++) {
             for (int j = 1; j<=N; j++) {
-                count = 0;
-                if(map[i][j] == 1 && !visited[i][j]) {
-                    dfs(i,j);
-                    arr.add(count);
-                    answer ++;
-
+                answer = 0;
+                if (!visited[i][j] && graph[i][j] == 1) {
+                    dfs(i, j);
+                    count ++;
+                    arr.add(answer);
                 }
             }
         }
 
-        //3.출력
-        bw.write(String.valueOf(answer));
-        bw.newLine();
-
+        //3. 오름차순 정렬
         Collections.sort(arr);
-        for (int a : arr) {
-            bw.write(String.valueOf(a) + "\n");
+
+        //4.출력
+        bw.write(String.valueOf(count));
+        bw.newLine();
+        for (int A : arr) {
+            bw.write(String.valueOf(A));
+            bw.newLine();
         }
         bw.close();
         br.close();
-
-
     }
 
     private static void dfs(int y, int x) {
+        //1. 방문 처리
         visited[y][x] = true;
-        count += 1;
-
-        for (int i = 0; i<4; i++) {
-            int newX = x + dirX[i];
-            int newY = y + dirY[i];
-
-            if (map[newY][newX] == 1 && visited[newY][newX] == false) {
-                dfs(newY,newX);
-
+        answer ++;
+        //2. 상하좌우 돌기
+        for (int i = 0; i < 4; i++) {
+            int newX = dirX[i] + x;
+            int newY = dirY[i] + y;
+            if (visited[newY][newX] == false && graph[newY][newX] == 1) {
+                dfs(newY, newX);
             }
+
         }
 
-
-
-
-
     }
-
-
 }
