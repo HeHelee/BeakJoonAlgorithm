0c33862446f1194ff2590c1118e0643cb3af2764
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-05-19 03:08:15 +0900

    [Silver I] Title: 미로 탐색, Time: 188 ms, Memory: 16812 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,69 +1,80 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+//현재 위치를 파악할 때 사용
+class Point {
+    public int x, y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Main {
+    static int MAX = 100+10;
+    static int [][] board, dis;
+    static int N,M;
+    static int dx[] = {-1,0,1,0};
+    static int dy[] = {0,1,0,-1};
+    public static void main(String[] args) throws IOException {
+        //1은 이동, 0은 이동 불가
+        //(1,1)에서 출발 -> (N,M)의 최소 칸수
+        //0.입력 및 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static int N, M;
-	static int[][] map;
-	static boolean[][] visited; //방문 체크
-	
-	static int cnt = 1; //출발 지점 (1,1)은 항상 1로 주어짐
-	
-	static int[] dy = {1,0,-1,0};
-	static int[] dx = {0,1,0,-1};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-	    N = Integer.parseInt(st.nextToken());
-	    M = Integer.parseInt(st.nextToken());
-	    
-	    map = new int[N][M];
-	    for (int i = 0; i<N; i++) {
-	    	String s = br.readLine();
-	    	for (int j = 0; j<M; j++) {
-	    		map[i][j] = s.charAt(j) - '0';
-	    	}
-	    }
-	    visited = new boolean[N][M];
-	    visited[0][0] = true;
-	    bfs(0,0);
-	    System.out.println(map[N-1][M-1]);
-	 
-	}
+        //0-1. 행과 열 입력
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-	private static void bfs(int x, int y) {
-		Queue<int []> q = new LinkedList<>();
-		q.add(new int[] {x,y});
-		
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			int nx = now[0];
-			int ny = now[1];
-			
-			for(int i = 0; i<4; i++) {
-				int nextX = nx + dx[i];
-				int nextY = ny + dy[i];
-				
-				if(nextX>=0 && nextX < N && nextY>=0 && nextY < M)
-				{
-					if(map[nextX][nextY] == 1 && !visited[nextX][nextY])
-					{
-						q.add(new int[] {nextX, nextY});
-						visited[nextX][nextY] = true;
-						map[nextX][nextY] = map[nx][ny]+1;
-					}
-				}
-				
-			}
-		}
-		
-	}
+        //0-2.board,dis 초기화
+        board = new int[MAX][MAX];
+        dis = new int[MAX][MAX];
 
-	
+        //0-3 값을 입력
+        for (int i = 1; i<=N; i++) {
+                String input[] = br.readLine().split("");
+                for (int j = 1; j <= M; j++) {
+                    board[i][j] = Integer.parseInt(input[j-1]);
+                }
+        }
+
+        //1.BFS 호출
+        //(1,1)에서 출발
+        BFS(1,1);
+
+        //2.출력 - 최단 거리 값
+        bw.write(String.valueOf(dis[N][M]));
+        bw.close();
+        br.close();
+    }
+
+    private static void BFS(int x, int y) {
+        //0.큐 생성
+        Queue<Point> q = new LinkedList<>();
+        //1.값 넣기
+        q.offer(new Point(x,y));
+        //2.시작점의 거리를 1로 설정, 1로 설정하면서 재방문 방지
+        dis[x][y] = 1;
+    
+        while (!q.isEmpty()) {
+            //3.팝
+            Point tmp = q.poll();
+            //4.상하좌우로 움직이기
+            for (int i = 0; i < 4; i++) {
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+                //5. 나가는 거 방지
+                if (nx >= 1 && nx <= N && ny >= 1 && ny <= M && board[nx][ny] == 1 && dis[nx][ny] == 0) {
+                    board[nx][ny] = 1;
+                    q.offer(new Point(nx,ny));
+                    dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
+                }
+
+            }
+        }
+    }
 }
