c36bd71e0f27024253b49b870a2f0643afeccb0f
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-12-04 10:54:22 +0900

    Title: 합을 복사하기, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241204/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       int a = 1, b = 2, c = 3;
+       a += b += c;
+       c = a;
+       b = a;
+
+       System.out.println(a + " " + b +" " +c );
+       
+
+    }
+}
