64d08965d9399d7b8b2b19f8c104feaa06aa8075
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-12-09 23:58:15 +0900

    Title: 비교 연산, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241209/비교 연산/comparison-operator.java
+++ 241209/비교 연산/comparison-operator.java
@@ -5,25 +5,25 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        if (a>b) {
-            System.out.println(1);
-        }else {
-            System.out.println(0);
-        }
-
         if (a>=b) {
             System.out.println(1);
         }else {
             System.out.println(0);
         }
 
-        if (a<b) {
+        if (a>b) {
             System.out.println(1);
         }else {
             System.out.println(0);
         }
 
         if (a<=b) {
+            System.out.println(1);
+        }else {
+            System.out.println(0);
+        }
+
+        if (a<b) {
             System.out.println(1);
         }else {
             System.out.println(0);
