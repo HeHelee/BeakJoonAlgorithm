4ebf7f2e7a0cee3f60f406665a62216f0d8a22d9
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-12-10 17:01:56 +0900

    Title: 중앙값 구하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 241210/중앙값 구하기/find-the-median.java
+++ 241210/중앙값 구하기/find-the-median.java
@@ -7,25 +7,19 @@ public class Main {
         int c = sc.nextInt();
 
         //중앙값 구하기 
-        // a가 b보다 클경우
-        // 26 22 21 or 26 22 30
-        if (a >= b) {
-            if (a >= c) {
-                System.out.println(b);
-            }else {
-                System.out.println(a);
-            }
-        }
-        // b가 a보다 클 경우
-        // 22 26 30 or 22 26 21
-        else {
-            if (a <= c) {
-                System.out.println(a);
-            }else {
-                System.out.println(b);
-            }
+        //22 26 30 A < B and B < C => B
+        //30 26 22 A > B and B > C => B
+        //26 22 30 A > B and A < C => A
+        //26 30 22 A < B and A < B => A
+        //22 30 26 A < B and B > C => C
 
 
+        if ((b <= a && a <= c) || (c <= a && a <= b)) {
+            System.out.println(a);
+        }else if ((a <= b && b <= c) || (b <= a && c <= b)) {
+            System.out.println(b);
+        }else {
+            System.out.println(c);
         }
     }
 }
