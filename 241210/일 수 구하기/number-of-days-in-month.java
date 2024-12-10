b645fc063521d819d3143bf006ec52306bad5f09
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-12-10 14:54:28 +0900

    Title: 일 수 구하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241210/일 수 구하기/number-of-days-in-month.java
+++ 241210/일 수 구하기/number-of-days-in-month.java
@@ -13,17 +13,17 @@ public class Main {
 
         if (n == 2) {
             System.out.println("28");
-        }else if (n >= 7) {
-            if (n % 2 == 0) {
+        }else if (n <= 7) {
+            if (n % 2 == 1) {
                 System.out.println("31");
             }else {
                 System.out.println("30");
             }
         }else {
             if (n % 2 == 0) {
-                System.out.println("30");
-            }else {
                 System.out.println("31");
+            }else {
+                System.out.println("30");
             }
         }
 
