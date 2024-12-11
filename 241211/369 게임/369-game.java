e16f7c9b08257b5a6240255db49d2d1cd756b097
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-12-11 16:48:58 +0900

    Title: 369 게임, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 241211/369 게임/369-game.java
+++ 241211/369 게임/369-game.java
@@ -9,7 +9,7 @@ public class Main {
             //수가 3의 배수
             if (i % 3 == 0) {
                 System.out.print(0 + " "); 
-            } else if (i % 10 == 3 || i % 6 == 6 || i % 9 == 9){
+            } else if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9){
                 System.out.print(0 + " ");
             }else if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
                 System.out.print(0 + " ");
