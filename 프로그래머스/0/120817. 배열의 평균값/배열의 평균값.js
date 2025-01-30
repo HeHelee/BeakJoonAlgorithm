49e7db332bba8a021adc1654c58aef8e424b9610
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2025-01-30 15:20:39 +0900

    [level 0] Title: 배열의 평균값, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    let answer = 0;
+    // 구하는 것 : 원소의 평균값을 구하기
+    // 배열의 값을 다 더해야 함.
+    // 더한 값을 개수만큼 나눠줘야 함.
+    let sum = 0;
+    for (number of numbers) {
+        sum += number;
+    }
+    answer = sum / numbers.length;
+    
+    return answer;
+}
