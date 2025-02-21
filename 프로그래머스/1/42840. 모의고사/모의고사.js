aa01f6d6a7e6617a7631cb3465241ca789a3057e
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2025-02-21 19:29:34 +0900

    [level 1] Title: 모의고사, Time: 13.90 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,28 +1,26 @@
 function solution(answers) {
-    // 문제 분석 : 정답 배열 answers가 주어졌을 때, 가장 많이 문제를 맞힌 사람을 배열에 넣어야 함.
-    // 1. patterns 배열 생성 => 정답 배열과 비교해야 해서 필요함.
+    //1. 수포자의 정답 배열을 나열하기
     const patterns = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]];
     
-    //2. 점수를 저장할 배열 추가
+    //2. 점수를 담는 배열
     const scores = [0,0,0];
-    //2. 정답 배열과 패턴 배열과 비교해야 함.
-    for (const [i, answer] of answers.entries()) {
-        for (const [j, pattern] of patterns.entries()) {
-            //3. 비교를 해서 맞으면 정답을 올려줘야 하고 각각의 점수를 저장해야 함.
-            if (answer == pattern[i%pattern.length]) {
-                scores[j] += 1;
-            }   
-        }
-    }
-    //4. 최대값을 저장
-    const highestScore = Math.max(...scores);
     
-    //5. 최대값과 일치하는 사람을 출력
-    const maxScore = []
-    for (let i = 0; i < scores.length; i++) {
-        if (highestScore === scores[i]) {
-            maxScore.push(i+1);
+    //3. 답과 각각의 수포자의 답안을 비교해서 scores 배열에 점수 올리기
+    for (const [i, answer] of answers.entries()) {
+        for (const [j,pattern] of patterns.entries()) {
+            if (answer === pattern[i % pattern.length]) {
+                scores[j] += 1;
+            }
         }
     }
-    return maxScore;
+    
+    //4. 점수 배열 중 가장 많이 맞은 사람의 인덱스 번호 + 1을 내보내기
+    const maxScore = Math.max(...scores);
+    
+    const answer = scores.map
+    ((scores,index) => scores === maxScore ? index+1 : null)
+    .filter(index => index !== null)
+    
+    
+    return answer;
 }
