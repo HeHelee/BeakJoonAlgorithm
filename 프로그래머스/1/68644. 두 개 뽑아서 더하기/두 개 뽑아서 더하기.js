4e7a75ca7b2fbe76f088aa9daca339d16106ac69
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2025-02-21 16:06:59 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.56 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,14 +1,20 @@
 function solution(numbers) {
-    var answer = [];
-    // 1. 배열에서 모든 경우의 수를 구한다.
+    // 정답 배열 : 오름 차순으로 정렬한 배열
+    // 1. 배열을 돌아야 함. (조건은 해당 인덱스보다 크게)
+    // 2. 해당 값을 다음 값과 더해서 새로운 배열에 넣기
+    // 3. 새로운 배열에서 중복 값 제거하고 오름차순으로 정렬해서 반환하기
+    
+    let arr = new Array();
+    let sum = 0;
+    
     for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            answer.push(numbers[i] + numbers[j]);
-        }
+       for (let j = i + 1; j < numbers.length; j++) {
+          sum = numbers[i] + numbers[j];
+          arr.push(sum);
+       }
     }
-    // 2. 중복값을 제거한다.
-    answer = [...new Set(answer)];
-    // 3. 오름차순으로 정렬한다.
+    
+    var answer = [...new Set(arr)];
     answer.sort((a,b) => a-b);
     return answer;
 }
