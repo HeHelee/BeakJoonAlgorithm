7a36d02c0448e89b0a949841c7f5343c76a1917c
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2025-03-02 20:33:19 +0900

    [level 2] Title: 방문 길이, Time: 1.24 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,46 +1,46 @@
-function move (x, y, dir) {
-        switch(dir) {
-            case "U":
-                return [x, y+1];
-            case "D":
-                return [x, y-1];
-            case "R":
-                return [x+1, y];
-            case "L":
-                return [x-1, y];
-        }
+function isValid(nx, ny) {
+    //3. 좌표를 넘어가면 안됨. => 함수로 빼기
+    return (nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5);
 }
-    
-function isValid(nx,ny) {
-   return nx <= 5 && nx >= -5 && ny <= 5 && ny >= -5
 
+function move (x,y,dir) {
+    //1. U,D,R,L 의 값을 정의해 줘야 함. => 함수로 빼기
+    switch(dir) {
+        case "U":
+            return [x,y+1];
+        case "D":
+            return [x,y-1];
+        case "R":
+            return [x+1,y]
+        case "L":
+            return [x-1,y];
+    }
 }
+
 function solution(dirs) {
-    // 구해야 하는 것 : 최초로 간 경로 길이
-    // 조건1 : 중복된 조건이 없어야 함.
-    // 조건2 : 범위를 벗어나면 안됨.
-    
-    // 원점 
+    // 1. 시작점 정의 
     let x = 0;
     let y = 0;
-    const visited = new Set();
     
-    // 이동 거리를 계산해야 함.
+    const answer = new Set();
+    
+    //4. 배열을 각각을 쪼개야 함.
     for (const dir of dirs) {
-        const [nx, ny] = move(x,y,dir);
+        //4-1. 쪼갠 후에 호출
+        const [nx,ny] = move(x,y,dir);
         
-    // 유효한지 계산
-    if (!isValid(nx,ny)) {
-        continue;
-    }
+        //4-2. 값이 유효한지 확인
+        if (!isValid(nx,ny)) {
+            continue;
+        }
+        
+        //5. 거리 연결하기
+        answer.add(`${x}${y}${nx}${ny}`);
+        answer.add(`${nx}${ny}${x}${y}`);
     
-    // 방문 거리 값 넣어주기 
-    // 방향성이 없기 때문에 A->B B->A 추가하기
-    visited.add(`${x}${y}${nx}${ny}`);
-    visited.add(`${nx}${ny}${x}${y}`);
-        
-    // 배열 값 업데이트
-    [x,y] = [nx,ny];
-  }
-    return visited.size/2;
+        //6. 좌표 업데이트
+        [x,y] = [nx,ny];
+    }
+   
+    return answer.size/2;
 }
