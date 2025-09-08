71b12146b00118c66c1e5db472daea42227f6002
Kuheewon <selly3109@naver.com>
2025-09-09 00:56:43 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9612 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let input1 = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+let c = Number(input1[0]);
+let d = Number(input1[1]);
+
+console.log(c + d);
+console.log(c - d);
+console.log(c * d);
+console.log(parseInt(c / d));
+console.log(c % d);
