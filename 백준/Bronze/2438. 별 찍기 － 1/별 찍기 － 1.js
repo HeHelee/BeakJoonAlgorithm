9ffedf18ade9eb86141e406c8710b8b139768ab7
Kuheewon <selly3109@naver.com>
2026-03-05 11:00:56 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 92 ms, Memory: 9516 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,8 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let a = parseInt(input[0]);
+
+for (let i = 1; i <= a; i++) {
+  console.log("*".repeat(i));
+}
