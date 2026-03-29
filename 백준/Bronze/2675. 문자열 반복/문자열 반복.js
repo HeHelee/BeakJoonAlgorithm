581a3818a1f64a7baa59e8a596f60ed73c88d926
Kuheewon <selly3109@naver.com>
2026-03-29 23:15:36 +0900

    [Bronze II] Title: 문자열 반복, Time: 84 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -3,14 +3,12 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let N = Number(input[0]);
 
-for (let i = 0; i < N; i++) {
-  let [A, B] = input[i + 1].split(" ");
+for (let i = 1; i <= N; i++) {
+  let [A, B] = input[i].split(" ");
   // A만큼 B의 문자열 각각을 반복해야 함.
   let result = "";
-  for (let j = 0; j < B.length; j++) {
-    for (let k = 0; k < A; k++) {
-      result += B[j];
-    }
+  for (let j = 0; j <= B.length; j++) {
+    result += B.charAt(j).repeat(A);
   }
   console.log(result);
 }
